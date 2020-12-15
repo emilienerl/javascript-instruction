fa8b1786415906423141761dee5f6e30af573b13
Emilie Nerl <emilie.nerl1@gmail.com>
2020-12-14 21:13:22 -0500

    javascript class instruction and labs

---
+++ chapter1.js
@@ -0,0 +1,9 @@
+// console.log("Agnes" || "user");
+// console.log("Agnes" == true);
+
+let mood = "light";
+console.log(typeof mood);
+console.log(mood);
+mood = 5;
+console.log(typeof mood);
+console.log(mood);
