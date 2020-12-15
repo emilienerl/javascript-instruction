9c5a75d0ac3e932c05592b217f90aef0e50f10ec
Emilie Nerl <emilie.nerl1@gmail.com>
2020-12-14 21:24:27 -0500

    more class work

--- chapter1.js
+++ chapter1.js
@@ -1,9 +1,12 @@
 // console.log("Agnes" || "user");
 // console.log("Agnes" == true);
 
-let mood = "light";
-console.log(typeof mood);
-console.log(mood);
-mood = 5;
-console.log(typeof mood);
-console.log(mood);
+// let mood = "light";
+// console.log(typeof mood);
+// console.log(mood);
+// mood = 5;
+// console.log(typeof mood);
+// console.log(mood);
+
+let myStr = "Hello";
+console.log(myStr[3]);
