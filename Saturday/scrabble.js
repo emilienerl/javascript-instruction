7c0a906becebac1bd1500690f111f1f651218c8b
Emilie Nerl <emilie.nerl1@gmail.com>
2020-12-19 08:32:50 -0500

    saturday work

---
+++ Saturday/scrabble.js
@@ -0,0 +1,23 @@
+let tiles = [
+    { tile: "N", score: 1 },
+    { tile: "K", score: 5 },
+    { tile: "Z", score: 10 },
+    { tile: "X", score: 8 },
+    { tile: "D", score: 2 },
+    { tile: "A", score: 1 },
+    { tile: "E", score: 1 }
+  ];
+
+  function maximumScore(hand) {
+      let total = 0;
+
+      for (let tile of hand) {
+          total += tile.score;
+      }
+      return total;
+  }
+  let total = maxiumScore(tiles);
+  console.log(total);
+
+total = maximumScore([{tile: 'B', score: 2}, {tile: 'V', score: 4},  {tile: 'F', score: 4}, {tile: 'U', score: 1}, {tile: 'D', score: 2}, {tile: 'O', score: 1}, {tile: 'U', score: 1}]), 15;
+console.log(total);
